98e89c463c52d56d39ee700a8c18bb350a58aecd
Lucas Busso <lucasxbusso@gmail.com>
2023-07-30 21:57:25 -0300

    index changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import { AppRouter } from "./Router";
 import { NotificationProvider } from "./context/notification.context";
 import { Suspense } from "react";
 import { Spinner } from "./common/Spinner";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { ThemeConfig } from "./config/theme.config";
 
 function App() {
   return (
     <NotificationProvider>
       <BrowserRouter>
         <Suspense fallback={<Spinner />}>
-          <AppRouter />
+          <Provider store={store}>
+            <ThemeConfig>
+              <AppRouter />
+            </ThemeConfig>
+          </Provider>
         </Suspense>
       </BrowserRouter>
     </NotificationProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ThemeConfig } from "./config/theme.config";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeConfig>
-        <App />
-      </ThemeConfig>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
